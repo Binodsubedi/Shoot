573a707f9bcf296df2c3e201d573b820210a2e28
dell <binodsubedi125@gmail.com>
2022-06-18 14:20:57 +0545

    Added the dashboard upload page

--- src/components/dashboardComp/dashboardNav.tsx
+++ src/components/dashboardComp/dashboardNav.tsx
@@ -4,14 +4,14 @@ const dashboardNav = ({name}) => {
   return (
     <div className={name}>
     <div className="DNav--mainItems">
-    <div className="DNav--mainItems__logo">logo</div>
+    <div className="DNav--mainItems__logo"></div>
     <div className="DNav--mainItems__links">
         <a href="" className="DNav--mainItems__links-1 DNav--link-1">Upload</a>
         <a href="" className="DNav--mainItems__links-2 DNav--link-2">Video</a>
         <a href="" className="DNav--mainItems__links-3 DNav--link-3">Pictures</a>
     </div>
 </div>
-<div className="DNav--profile">Profile</div></div>
+<div className="DNav--profile"></div></div>
   )
 }
 
--- src/components/dashboardComp/dashboardUploadBody.tsx
+++ src/components/dashboardComp/dashboardUploadBody.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const dashboardUploadBody = ({name}) => {
+const DashboardUploadBody = ({name}) => {
+
+  const [FileName, SetFileName] = useState('');
+
+  const changeAction = (e:any)=>{
+
+    const val = e.target.value;
+
+    let valLast = val.split('\\')
+
+    console.log(valLast)
+
+
+    SetFileName(valLast[valLast.length -1])
+
+  }
+
   return (
     <div className={name}>
-        Dashboard-Body
+        <form className='DUBody--mainContainer'>
+          <input type="file" className='DUBody--mainContainer__file' onChange={(e)=>changeAction(e)} />
+          <input type="text" placeholder='Name' value={FileName} onChange={(e)=>SetFileName(e.target.value)} required />
+          <button >Submit</button>
+        </form>
     </div>
   )
 }
 
-export default dashboardUploadBody
+export default DashboardUploadBody
