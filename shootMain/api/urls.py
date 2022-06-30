25b0a1e78324b075276eff3a17d59a21a8dda786
dell <binodsubedi125@gmail.com>
2022-06-30 12:46:24 +0545

    minor changes

--- shootMain/api/urls.py
+++ shootMain/api/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('user/<int:pk>', UserSpecificEP.as_view(), name='user'),
     path('payment', PaymentListEP.as_view(), name='payment-list'),
     path('payment/<int:pk>', PaymentEP.as_view(), name='payment'),
-    path('photos/', PhotosListEP.as_view(), name='photo-list'),
+    path('photos', PhotosListEP.as_view(), name='photo-list'),
     path('photos/<int:pk>', PhotoEP.as_view(), name='photo'),
-    path('videos/', VideoListEP.as_view(), name='video-list'),
+    path('videos', VideoListEP.as_view(), name='video-list'),
     path('videos/<int:pk>', VideoEP.as_view(), name='video'),
 ]
